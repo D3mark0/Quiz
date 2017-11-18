97a0aa393d88a6965e4b98fb53f6391e6c9e7866
fogrest <pwnscr@gmail.com>
2017-11-18 14:52:29 +0700

    add mainform treeview

--- Quiz/Quiz/MainForm.Designer.cs
+++ Quiz/Quiz/MainForm.Designer.cs
@@ -28,21 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("ОВП.01 т.1 Общевоинские уставы ВС РФ. Права, обязанности и ответственность военно" +
+        "служащих");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("ОВП.01 т.2 Взаимоотношения между военнослужащими");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("ОВП.01 т.3, з.1 Внутренний порядок и суточный наряд");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("ОВП.01 т.4, з.1 Воинская дисциплина");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("ОВП.01 т.4, з.2 Дисциплинарная практика");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("ОВП.01 т.5 Организация и несение гарнизонной службы");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("ОВП.01 т.6, з.1 Организация и несение караульной службы");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("ОВП.01. Общевоинские уставы", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6,
+            treeNode7});
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("ОВП.02 т.1, з.1 Строевые приемы без оружия на месте");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("ОВП.02 т.1, з.2 Движение строевым шагом");
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("ОВП.02 т.1, з.3 Строевые приемы без оружия в движении");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("ОВП.02 т.1, з.4 Проверка одиночной строевой подготовки");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("ОВП.02 т.2, з.1 Строевые приёмы с оружием на месте");
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("ОВП.02 т.2, з.2 Строевые приемы с оружием в движении");
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("ОВП.02 т.3, з.1 Строевое слаживание отделения");
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("ОВП.02 т.3, з.2 Строевое слаживание взвода");
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("ОВП.02 т.3, з.3 Способы передвижения на поле боя");
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("ОВП.02 т.4, з.1 Основы методики строевой подготовки");
+            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("ОВП.02 т.4, з.2 Проведение занятий по строевой подготовки");
+            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("ОВП.02. Строевая подготовка", new System.Windows.Forms.TreeNode[] {
+            treeNode9,
+            treeNode10,
+            treeNode11,
+            treeNode12,
+            treeNode13,
+            treeNode14,
+            treeNode15,
+            treeNode16,
+            treeNode17,
+            treeNode18,
+            treeNode19});
+            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("ОВП.03 т.1, з.1 Общие сведения о стрелковом оружии");
+            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("ОВП.03 т.1, з.2 Разборка и сборка стрелкового оружия");
+            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("ОВП.03 т.1, з.3 Ручные гранаты");
+            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("ОВП.03 т.2 Основы, приемы и правила стрельбы из стрелкового оружия");
+            System.Windows.Forms.TreeNode treeNode25 = new System.Windows.Forms.TreeNode("ОВП.03 т.3, з.1 Организация эксплуатации стрелкового оружия и ручных гранат");
+            System.Windows.Forms.TreeNode treeNode26 = new System.Windows.Forms.TreeNode("ОВП.03 т.4, з.1 Стрелковые тренировки. Выполнение учебных стрельб из стрелкового " +
+        "оружия и упражнений в метании ручных гранат в пешем порядке");
+            System.Windows.Forms.TreeNode treeNode27 = new System.Windows.Forms.TreeNode("ОВП.03. Огневая подготовка", new System.Windows.Forms.TreeNode[] {
+            treeNode21,
+            treeNode22,
+            treeNode23,
+            treeNode24,
+            treeNode25,
+            treeNode26});
+            System.Windows.Forms.TreeNode treeNode28 = new System.Windows.Forms.TreeNode("ОВП.04 т.1 Местность как элемент боевой обстановки");
+            System.Windows.Forms.TreeNode treeNode29 = new System.Windows.Forms.TreeNode("ОВП.04 т.2 Топографические карты и чтение их");
+            System.Windows.Forms.TreeNode treeNode30 = new System.Windows.Forms.TreeNode("ОВП.04 т.4 Рабочая карта командира");
+            System.Windows.Forms.TreeNode treeNode31 = new System.Windows.Forms.TreeNode("ОВП.04. Топография", new System.Windows.Forms.TreeNode[] {
+            treeNode28,
+            treeNode29,
+            treeNode30});
+            System.Windows.Forms.TreeNode treeNode32 = new System.Windows.Forms.TreeNode("Раздел 1");
+            System.Windows.Forms.TreeNode treeNode33 = new System.Windows.Forms.TreeNode("Раздел 2");
+            System.Windows.Forms.TreeNode treeNode34 = new System.Windows.Forms.TreeNode("Раздел 3");
+            System.Windows.Forms.TreeNode treeNode35 = new System.Windows.Forms.TreeNode("Раздел 4");
+            System.Windows.Forms.TreeNode treeNode36 = new System.Windows.Forms.TreeNode("Полное тестирование");
+            System.Windows.Forms.TreeNode treeNode37 = new System.Windows.Forms.TreeNode("Тестирование", new System.Windows.Forms.TreeNode[] {
+            treeNode32,
+            treeNode33,
+            treeNode34,
+            treeNode35,
+            treeNode36});
+            System.Windows.Forms.TreeNode treeNode38 = new System.Windows.Forms.TreeNode("Посмотреть вопросы");
+            System.Windows.Forms.TreeNode treeNode39 = new System.Windows.Forms.TreeNode("Редактирование вопросов");
+            System.Windows.Forms.TreeNode treeNode40 = new System.Windows.Forms.TreeNode("Вопросы", new System.Windows.Forms.TreeNode[] {
+            treeNode38,
+            treeNode39});
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
-            // Form1
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "ovp11";
+            treeNode1.Text = "ОВП.01 т.1 Общевоинские уставы ВС РФ. Права, обязанности и ответственность военно" +
+    "служащих";
+            treeNode2.Name = "ovp12";
+            treeNode2.Text = "ОВП.01 т.2 Взаимоотношения между военнослужащими";
+            treeNode3.Name = "ovp13";
+            treeNode3.Text = "ОВП.01 т.3, з.1 Внутренний порядок и суточный наряд";
+            treeNode4.Name = "ovp141";
+            treeNode4.Text = "ОВП.01 т.4, з.1 Воинская дисциплина";
+            treeNode5.Name = "ovp142";
+            treeNode5.Text = "ОВП.01 т.4, з.2 Дисциплинарная практика";
+            treeNode6.Name = "ovp15";
+            treeNode6.Text = "ОВП.01 т.5 Организация и несение гарнизонной службы";
+            treeNode7.Name = "ovp16";
+            treeNode7.Text = "ОВП.01 т.6, з.1 Организация и несение караульной службы";
+            treeNode8.Name = "ovp1";
+            treeNode8.Text = "ОВП.01. Общевоинские уставы";
+            treeNode9.Name = "ovp211";
+            treeNode9.Text = "ОВП.02 т.1, з.1 Строевые приемы без оружия на месте";
+            treeNode10.Name = "ovp212";
+            treeNode10.Text = "ОВП.02 т.1, з.2 Движение строевым шагом";
+            treeNode11.Name = "ovp213";
+            treeNode11.Text = "ОВП.02 т.1, з.3 Строевые приемы без оружия в движении";
+            treeNode12.Name = "ovp214";
+            treeNode12.Text = "ОВП.02 т.1, з.4 Проверка одиночной строевой подготовки";
+            treeNode13.Name = "ovp221";
+            treeNode13.Text = "ОВП.02 т.2, з.1 Строевые приёмы с оружием на месте";
+            treeNode14.Name = "ovp222";
+            treeNode14.Text = "ОВП.02 т.2, з.2 Строевые приемы с оружием в движении";
+            treeNode15.Name = "ovp231";
+            treeNode15.Text = "ОВП.02 т.3, з.1 Строевое слаживание отделения";
+            treeNode16.Name = "ovp232";
+            treeNode16.Text = "ОВП.02 т.3, з.2 Строевое слаживание взвода";
+            treeNode17.Name = "ovp233";
+            treeNode17.Text = "ОВП.02 т.3, з.3 Способы передвижения на поле боя";
+            treeNode18.Name = "ovp241";
+            treeNode18.Text = "ОВП.02 т.4, з.1 Основы методики строевой подготовки";
+            treeNode19.Name = "ovp242";
+            treeNode19.Text = "ОВП.02 т.4, з.2 Проведение занятий по строевой подготовки";
+            treeNode20.Name = "ovp2";
+            treeNode20.Text = "ОВП.02. Строевая подготовка";
+            treeNode21.Name = "ovp311";
+            treeNode21.Text = "ОВП.03 т.1, з.1 Общие сведения о стрелковом оружии";
+            treeNode22.Name = "ovp312";
+            treeNode22.Text = "ОВП.03 т.1, з.2 Разборка и сборка стрелкового оружия";
+            treeNode23.Name = "ovp313";
+            treeNode23.Text = "ОВП.03 т.1, з.3 Ручные гранаты";
+            treeNode24.Name = "ovp32";
+            treeNode24.Text = "ОВП.03 т.2 Основы, приемы и правила стрельбы из стрелкового оружия";
+            treeNode25.Name = "ovp33";
+            treeNode25.Text = "ОВП.03 т.3, з.1 Организация эксплуатации стрелкового оружия и ручных гранат";
+            treeNode26.Name = "ovp34";
+            treeNode26.Text = "ОВП.03 т.4, з.1 Стрелковые тренировки. Выполнение учебных стрельб из стрелкового " +
+    "оружия и упражнений в метании ручных гранат в пешем порядке";
+            treeNode27.Name = "ovp3";
+            treeNode27.Text = "ОВП.03. Огневая подготовка";
+            treeNode28.Name = "ovp41";
+            treeNode28.Text = "ОВП.04 т.1 Местность как элемент боевой обстановки";
+            treeNode29.Name = "ovp42";
+            treeNode29.Text = "ОВП.04 т.2 Топографические карты и чтение их";
+            treeNode30.Name = "ovp44";
+            treeNode30.Text = "ОВП.04 т.4 Рабочая карта командира";
+            treeNode31.Name = "ovp4";
+            treeNode31.Text = "ОВП.04. Топография";
+            treeNode32.Name = "testovp1";
+            treeNode32.Text = "Раздел 1";
+            treeNode33.Name = "testovp2";
+            treeNode33.Text = "Раздел 2";
+            treeNode34.Name = "testovp3";
+            treeNode34.Text = "Раздел 3";
+            treeNode35.Name = "testovp4";
+            treeNode35.Text = "Раздел 4";
+            treeNode36.Name = "testall";
+            treeNode36.Text = "Полное тестирование";
+            treeNode37.Name = "test";
+            treeNode37.Text = "Тестирование";
+            treeNode38.Name = "seequestion";
+            treeNode38.Text = "Посмотреть вопросы";
+            treeNode39.Name = "editquestion";
+            treeNode39.Text = "Редактирование вопросов";
+            treeNode40.Name = "questions";
+            treeNode40.Text = "Вопросы";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode8,
+            treeNode20,
+            treeNode27,
+            treeNode31,
+            treeNode37,
+            treeNode40});
+            this.treeView1.Size = new System.Drawing.Size(582, 366);
+            this.treeView1.TabIndex = 0;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(606, 390);
+            this.Controls.Add(this.treeView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
 
