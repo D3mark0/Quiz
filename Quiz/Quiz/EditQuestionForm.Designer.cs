0027b673c43d62c31cbd3733c2bf427e0472b96e
D3mark0 <striown@gmail.com>
2017-12-17 16:28:44 +0700

    edit questionsform and editquestionsform interface

--- Quiz/Quiz/EditQuestionForm.Designer.cs
+++ Quiz/Quiz/EditQuestionForm.Designer.cs
@@ -38,12 +38,15 @@
             this.buttonSave = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.groupBoxAnswers.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBoxQuestion
@@ -106,7 +109,7 @@
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(12, 290);
+            this.buttonSave.Location = new System.Drawing.Point(6, 19);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(75, 23);
             this.buttonSave.TabIndex = 3;
@@ -116,7 +119,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(578, 290);
+            this.button2.Location = new System.Drawing.Point(398, 16);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
@@ -132,16 +135,46 @@
             this.groupBox1.Controls.Add(this.radioButton1);
             this.groupBox1.Location = new System.Drawing.Point(12, 253);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(153, 37);
+            this.groupBox1.Size = new System.Drawing.Size(156, 52);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Правильный ответ";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(118, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(31, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(81, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(31, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(44, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(7, 14);
+            this.radioButton1.Location = new System.Drawing.Point(7, 19);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(31, 17);
             this.radioButton1.TabIndex = 0;
@@ -149,44 +182,24 @@
             this.radioButton1.Text = "1";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // groupBox2
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(44, 14);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(31, 17);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.Text = "2";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(81, 14);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(31, 17);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.Text = "3";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(116, 14);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(31, 17);
-            this.radioButton4.TabIndex = 3;
-            this.radioButton4.Text = "4";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.groupBox2.Controls.Add(this.buttonSave);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(174, 253);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(479, 52);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Опции";
             // 
             // EditQuestionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(665, 317);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBoxAnswers);
             this.Controls.Add(this.textBoxQuestion);
@@ -199,6 +212,7 @@
             this.groupBoxAnswers.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -220,5 +234,7 @@
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
--- Quiz/Quiz/QuestionsForm.Designer.cs
+++ Quiz/Quiz/QuestionsForm.Designer.cs
@@ -32,20 +32,24 @@
             System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("ОВП.02. Строевая подготовка");
             System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("ОВП.03. Огневая подготовка");
             System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("ОВП.04. Топография");
-
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.textBoxQuestion = new System.Windows.Forms.TextBox();
-            this.textBoxAnswers = new System.Windows.Forms.TextBox();
             this.buttonAdd = new System.Windows.Forms.Button();
             this.buttonEdit = new System.Windows.Forms.Button();
             this.buttonDelete = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBoxAnswers = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Location = new System.Drawing.Point(6, 19);
             this.treeView1.Name = "treeView1";
             treeNode5.Name = "1";
             treeNode5.Text = "ОВП.01. Общевоинские уставы";
@@ -60,35 +64,26 @@
             treeNode6,
             treeNode7,
             treeNode8});
-            this.treeView1.Size = new System.Drawing.Size(272, 452);
+            this.treeView1.Size = new System.Drawing.Size(263, 446);
             this.treeView1.TabIndex = 1;
             this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
             this.treeView1.Leave += new System.EventHandler(this.treeView1_Leave);
             // 
             // textBoxQuestion
             // 
-            this.textBoxQuestion.Location = new System.Drawing.Point(290, 30);
+            this.textBoxQuestion.Location = new System.Drawing.Point(299, 31);
             this.textBoxQuestion.Multiline = true;
             this.textBoxQuestion.Name = "textBoxQuestion";
             this.textBoxQuestion.ReadOnly = true;
-            this.textBoxQuestion.Size = new System.Drawing.Size(340, 190);
+            this.textBoxQuestion.Size = new System.Drawing.Size(325, 183);
             this.textBoxQuestion.TabIndex = 0;
-            // 
-            // textBoxAnswers
-            // 
-            this.textBoxAnswers.Location = new System.Drawing.Point(290, 239);
-            this.textBoxAnswers.Multiline = true;
-            this.textBoxAnswers.Name = "textBoxAnswers";
-            this.textBoxAnswers.ReadOnly = true;
-            this.textBoxAnswers.Size = new System.Drawing.Size(340, 190);
-            this.textBoxAnswers.TabIndex = 2;
             // 
             // buttonAdd
             // 
             this.buttonAdd.Enabled = false;
-            this.buttonAdd.Location = new System.Drawing.Point(290, 441);
+            this.buttonAdd.Location = new System.Drawing.Point(6, 19);
             this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.Size = new System.Drawing.Size(94, 23);
             this.buttonAdd.TabIndex = 3;
             this.buttonAdd.Text = "Добавить";
             this.buttonAdd.UseVisualStyleBackColor = true;
@@ -97,7 +92,7 @@
             // buttonEdit
             // 
             this.buttonEdit.Enabled = false;
-            this.buttonEdit.Location = new System.Drawing.Point(414, 441);
+            this.buttonEdit.Location = new System.Drawing.Point(122, 19);
             this.buttonEdit.Name = "buttonEdit";
             this.buttonEdit.Size = new System.Drawing.Size(94, 23);
             this.buttonEdit.TabIndex = 4;
@@ -108,49 +103,80 @@
             // buttonDelete
             // 
             this.buttonDelete.Enabled = false;
-            this.buttonDelete.Location = new System.Drawing.Point(555, 441);
+            this.buttonDelete.Location = new System.Drawing.Point(237, 19);
             this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.Size = new System.Drawing.Size(94, 23);
             this.buttonDelete.TabIndex = 5;
             this.buttonDelete.Text = "Удалить";
             this.buttonDelete.UseVisualStyleBackColor = true;
             this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.treeView1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(275, 471);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Разделы и вопросы";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.buttonAdd);
+            this.groupBox4.Controls.Add(this.buttonEdit);
+            this.groupBox4.Controls.Add(this.buttonDelete);
+            this.groupBox4.Location = new System.Drawing.Point(293, 428);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(337, 55);
+            this.groupBox4.TabIndex = 11;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Вопрос";
+            // 
+            // textBoxAnswers
+            // 
+            this.textBoxAnswers.Location = new System.Drawing.Point(299, 251);
+            this.textBoxAnswers.Multiline = true;
+            this.textBoxAnswers.Name = "textBoxAnswers";
+            this.textBoxAnswers.ReadOnly = true;
+            this.textBoxAnswers.Size = new System.Drawing.Size(325, 171);
+            this.textBoxAnswers.TabIndex = 2;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(290, 14);
+            this.label1.Location = new System.Drawing.Point(296, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 13);
-            this.label1.TabIndex = 6;
+            this.label1.TabIndex = 12;
             this.label1.Text = "Текст вопроса:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(290, 223);
+            this.label2.Location = new System.Drawing.Point(299, 232);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(103, 13);
-            this.label2.TabIndex = 7;
+            this.label2.TabIndex = 13;
             this.label2.Text = "Варианты ответов:";
             // 
             // QuestionsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(638, 472);
+            this.ClientSize = new System.Drawing.Size(638, 487);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.buttonDelete);
-            this.Controls.Add(this.buttonEdit);
-            this.Controls.Add(this.buttonAdd);
-            this.Controls.Add(this.textBoxAnswers);
             this.Controls.Add(this.textBoxQuestion);
-            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.textBoxAnswers);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "QuestionsForm";
             this.Text = "Вопросы";
             this.Load += new System.EventHandler(this.QuestionsForm_Load);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,10 +186,13 @@
 
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.TextBox textBoxQuestion;
-        private System.Windows.Forms.TextBox textBoxAnswers;
         private System.Windows.Forms.Button buttonAdd;
         private System.Windows.Forms.Button buttonEdit;
         private System.Windows.Forms.Button buttonDelete;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBoxAnswers;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
     }
